3d882236eec0faaf21f3f0e9a283c740e162c441
hyukjin0419 <21900771@handong.ac.kr>
2024-04-14 17:49:05 +0900

    lottie is on

--- mdc_100_series/lib/mypage.dart
+++ mdc_100_series/lib/mypage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class MyPage extends StatefulWidget {
   const MyPage({Key? key}) : super(key: key);
@@ -11,11 +12,12 @@ class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('mypage Page'),
-      ),
-      body: Center(
-        child: Text('This is a mypage page.'),
+      body: ListView(
+        children: [
+          // Load a Lottie file from a remote url
+          Lottie.network(
+              'https://raw.githubusercontent.com/xvrh/lottie-flutter/master/example/assets/Mobilo/A.json'),
+        ],
       ),
     );
   }

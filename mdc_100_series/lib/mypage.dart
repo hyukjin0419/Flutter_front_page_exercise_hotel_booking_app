d67992ef4b03299d70825f18fabd61dae13da8a4
hyukjin0419 <21900771@handong.ac.kr>
2024-04-14 20:12:55 +0900

    now stack is working. ready to do inwell part.

--- mdc_100_series/lib/mypage.dart
+++ mdc_100_series/lib/mypage.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
-import 'package:shrine/app.dart';
 import 'package:provider/provider.dart';
 import 'model/product.dart';
 import 'model/products_repository.dart';
+import 'app.dart';
 
 class MyPage extends StatefulWidget {
   const MyPage({Key? key}) : super(key: key);
@@ -15,6 +15,8 @@ class MyPage extends StatefulWidget {
 class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
+    var appState = context.read<MyAppState>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("My Page"),
@@ -47,40 +49,63 @@ class _MyPageState extends State<MyPage> {
                 Text('21900771'),
               ],
             ),
-            ListView(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              children: _buildListCards(context),
-            )
+            Consumer<MyAppState>(
+              builder: (context, myAppState, _) {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: myAppState.favorites.length,
+                  itemBuilder: (context, index) {
+                    final product = myAppState.favorites[index];
+                    return Center(
+                      child: Card(
+                        child: SizedBox(
+                          width: 300,
+                          height: 300,
+                          child: Stack(
+                            children: [
+                              Image.asset(
+                                product.assetName,
+                                package: product.assetPackage,
+                                width: 300,
+                                height: 300,
+                                fit: BoxFit.cover,
+                              ),
+                              Positioned(
+                                bottom: 16.0,
+                                left: 16.0,
+                                child: Container(
+                                  padding: EdgeInsets.all(8),
+                                  // color: Colors.white,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(product.name,
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                          )),
+                                      Text(product.address,
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                          )),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
           ],
         ),
       ),
     );
-  }
-
-  List<Card> _buildListCards(BuildContext context) {
-    List<Product> products = ProductsRepository.loadProducts(Category.all);
-
-    if (products.isEmpty) {
-      return const <Card>[];
-    }
-
-    final ThemeData theme = Theme.of(context);
-
-    return products.map((product) {
-      return Card(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Title'),
-              Text('descript'),
-            ],
-          ),
-        ),
-      );
-    }).toList();
   }
 }

08103535cf87ea55ef8091d522f5c3e57b199a8e
hyukjin0419 <21900771@handong.ac.kr>
2024-04-14 16:58:06 +0900

    lists are rendering in favorite.dart

--- mdc_100_series/lib/favorite.dart
+++ mdc_100_series/lib/favorite.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'model/product.dart';
+import 'app.dart';
 
-class FavoritePage extends StatefulWidget {
+class FavoritePage extends StatelessWidget {
   const FavoritePage({Key? key}) : super(key: key);
 
-  @override
-  _FavoritePageState createState() => _FavoritePageState();
-}
-
-class _FavoritePageState extends State<FavoritePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('favorite Page'),
+        title: Text('Favorites'),
       ),
-      body: Center(
-        child: Text('This is a favorite page.'),
+      body: Consumer<MyAppState>(
+        builder: (context, myAppState, _) {
+          return ListView.builder(
+            itemCount: myAppState.favorites.length,
+            itemBuilder: (context, index) {
+              final product = myAppState.favorites[index];
+              return ListTile(
+                title: Text(product.name),
+              );
+            },
+          );
+        },
       ),
     );
   }
